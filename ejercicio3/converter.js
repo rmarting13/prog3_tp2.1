3b9e27832172b886b2afdc1a8bad9cdca9244b00
rmarting13 <rmarting13@gmail.com>
2024-06-20 22:01:12 -0300

    ejercicio3

--- ejercicio3/converter.js
+++ ejercicio3/converter.js
@@ -6,11 +6,69 @@ class Currency {
 }
 
 class CurrencyConverter {
-    constructor() {}
+    constructor(apiUrl) {
+        this.apiUrl = apiUrl;
+        this.currencies = [];
+    }
 
-    getCurrencies(apiUrl) {}
+    async getCurrencies() {
+        try{
+            const resp = await fetch(`${this.apiUrl}/currencies`);
+            const data = await resp.json();
+            for(let curr in data){
+                this.currencies.push(
+                    new Currency(curr, data[curr])
+                );
+            }
+        }
+        catch(error){
+            alert(error);
+        }
+    }
 
-    convertCurrency(amount, fromCurrency, toCurrency) {}
+    async convertCurrency(amount, fromCurrency, toCurrency) {
+        try{
+            if(fromCurrency.code != toCurrency.code){
+                const request = `/latest?amount=${amount}&from=${fromCurrency.code}&to=${toCurrency.code}`
+                const resp = await fetch(`${this.apiUrl}${request}`);
+                const data = await resp.json();
+                return data['rates'][toCurrency.code];
+            }
+            else{
+                return Number(amount);
+            }
+            
+        }
+        catch(error){
+            return null;
+        }
+    }
+
+    async getHistoricalRateDifference(fromCurrency, toCurrency){
+        try{
+            const date = new Date();
+
+            //Obtención de la cotización de hoy:
+            const today = date.toISOString().split('T')[0];
+            let resp = await fetch(`${this.apiUrl}/${today}?from=${fromCurrency.code}&to=${toCurrency.code}`);
+            let data = await resp.json();
+            const todayRate = data['rates'][toCurrency.code];
+            
+            //Obtención de la cotización de ayer:
+            date.setDate(date.getDate()-1);
+            const yesterday = date.toISOString().split('T')[0];
+            resp = await fetch(`${this.apiUrl}/${yesterday}?from=${fromCurrency.code}&to=${toCurrency.code}`);
+            data = await resp.json();
+            const yesterdayRate = data['rates'][toCurrency.code];
+            
+            //Devuelve la diferencia entre las cotizaciones:
+            return todayRate - yesterdayRate;
+
+        }
+        catch(error){
+            return null;
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -35,6 +93,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         const toCurrency = converter.currencies.find(
             (currency) => currency.code === toCurrencySelect.value
         );
+        const difference = await converter.getHistoricalRateDifference(fromCurrency, toCurrency);
 
         const convertedAmount = await converter.convertCurrency(
             amount,
@@ -45,7 +104,8 @@ document.addEventListener("DOMContentLoaded", async () => {
         if (convertedAmount !== null && !isNaN(convertedAmount)) {
             resultDiv.textContent = `${amount} ${
                 fromCurrency.code
-            } son ${convertedAmount.toFixed(2)} ${toCurrency.code}`;
+            } son ${convertedAmount.toFixed(2)} ${toCurrency.code} 
+            ${(difference != 0)?'('+((difference > 0)?'+':'')+difference.toFixed(4)+' por unidad monetaria en relación a la cotización de ayer)':''}`;
         } else {
             resultDiv.textContent = "Error al realizar la conversión.";
         }
